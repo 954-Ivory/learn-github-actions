d276ebf9cf89e4ceb40cae25a7353755ffe71ebe
954 <510485871@qq.com>
2023-01-07 22:06:29 +0800

    chore: Test the python file.

---
+++ .github/actions-scripts/check_link.py
@@ -0,0 +1,23 @@
+import logging.handlers
+import os
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+if __name__ == "__main__":
+    logger.info(f"os.environ: {os.environ}")
+
+    # from githubkit import GitHub
+    # github = GitHub()
+    # github.rest
+    # github = GitHub(UnauthAuthStrategy())
